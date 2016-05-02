cf87b818911364776906e2a740e66402acc0c68e
Eintrag <vitavitae1@gmail.com>
2016-05-02 20:20:51 +0200

    Añadido toJSON() a IRankingEntry igual que en server

--- app/src/main/java/com/maco/clientejuegos/domain/IRankingEntry.java
+++ app/src/main/java/com/maco/clientejuegos/domain/IRankingEntry.java
@@ -1,8 +1,12 @@
 package com.maco.clientejuegos.domain;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public interface IRankingEntry {
     // Component element
     public String getEmailganador();
     public int getNumVictorias();
+    public JSONObject toJSON() throws JSONException;
 }
 
--- app/src/main/java/com/maco/clientejuegos/domain/RankingEntry.java
+++ app/src/main/java/com/maco/clientejuegos/domain/RankingEntry.java
@@ -1,17 +1,26 @@
 package com.maco.clientejuegos.domain;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class RankingEntry implements IRankingEntry{
-    // Leaf element
-    private String emailGanador;
-    private int numVictorias;
-    public RankingEntry(String emailganador, int numVictorias){
-        this.emailGanador = emailganador;
-        this.numVictorias = numVictorias;
-    }
-    public String getEmailganador(){
-        return emailGanador;
-    }
-    public int getNumVictorias(){
-        return numVictorias;
-    }
+// Leaf element
+	private String emailGanador;
+	private int numVictorias;
+	public RankingEntry(String emailganador, int numVictorias){
+		this.emailGanador = emailganador;
+		this.numVictorias = numVictorias;
+	}
+	public String getEmailganador(){
+		return emailGanador;
+	}
+	public int getNumVictorias(){
+		return numVictorias;
+	}
+	public JSONObject toJSON() throws JSONException {
+		JSONObject jso=new JSONObject();
+		jso.put("emailGanador", this.emailGanador);
+		jso.put("numVictorias", this.numVictorias);
+		return jso;
+	}
 }
